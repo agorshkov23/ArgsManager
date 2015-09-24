4f06c890c47296f30a085ceec9270c6e0ec4b7b2
Alexey Gorshkov <agorshkov23@yandex.ru>
2015-09-24 18:24:54 +0300

    Release ArgsManager 1.0

---
+++ ArgsManager/ArgsManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace AG23.ArgsManager
+{
+    public static class ArgsManager
+    {
+        public static bool GetArg(char name)
+        {
+            return Environment.GetCommandLineArgs().Any(arg => arg.TrimStart('/', '-').Equals($"{name}", StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public static string GetArg(string name, string @default = null)
+        {
+            var flag = false;
+            foreach (var arg in Environment.GetCommandLineArgs())
+            {
+                if (flag)
+                    return arg;
+                if (arg.TrimStart('/', '-').Equals(name, StringComparison.InvariantCultureIgnoreCase))
+                    flag = true;
+            }
+            return @default;
+        }
+    }
+}
